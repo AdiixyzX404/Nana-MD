5339ad77ffc0015d8112b5e3b7a77156e85a0b29
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-12-19 22:08:20 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,15 +4,14 @@ let handler = async (m) => {
 
 Hai 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
-┌〔 Donasi • Emoney 〕
-├🏧 081298108109 (OVO/Dana)
-└────
+60199782326 (Gopay)
+
 Berapapun donasi kalian akan sangat berarti 👍
 
 Arigatou!
 
 Contact person Owner:
-wa.me/6281390199407 (Owner)`)
+wa.me/60199782326 (Owner)`)
 }
 
 handler.help = ['donasi']
